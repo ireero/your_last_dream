f1318854b16a6d553ca6e7dda0390063b02bdeba
ireero <78822225+ireero@users.noreply.github.com>
2024-06-25 01:51:13 -0300

    Modificando a estilização da página

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def index():
-    today_date = datetime.today().strftime('%d-%m-%Y')
+    today_date = datetime.today().strftime('%d/%m/%Y')
     return render_template('index.html', today_date=today_date)
 
 @views.route('/register_dream', methods=['POST'])
