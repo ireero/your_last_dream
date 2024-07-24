6e7b9512a5177bbc1e74125c0020bb9c45bc27fa
ireero <78822225+ireero@users.noreply.github.com>
2024-07-24 02:32:03 -0300

    .

--- models.py
+++ models.py
@@ -2,11 +2,12 @@ from .app import db, app
 
 class Dreams(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text_dream = db.Column(db.Text, nullable=False)
-    model_response = db.Column(db.Text, nullable=False)
+    user_original_text_dream = db.Column(db.Text, nullable=False)
+    llm_text_resume = db.Column(db.Text, nullable=False)
+    llm_psicologic_analisys = db.Column(db.Text, nullable=False)
+    llm_creative_sugestions = db.Column(db.Text, nullable=False)
     date = db.Column(db.String(8), nullable=False)  # yyyymmdd
     time = db.Column(db.String(5), nullable=False)  # hh:mm
 
-# Crie as tabelas no banco de dados
 with app.app_context():
     db.create_all()
